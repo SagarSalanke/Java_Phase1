5840288981db6d0356f2e2affd2410be8e30a635
Sagar Rao Salanke P <sagarsalanke2@gmail.com>
2022-02-28 11:39:24 +0530

    first commit

---
+++ AccessModifiers/src/com/accessspecifier/defAccessSpecifier.java
@@ -0,0 +1,9 @@
+package com.accessspecifier;
+
+public class defAccessSpecifier {
+	void display() 
+    { 
+        System.out.println("You are using defalut access specifier"); 
+    } 
+
+}
---
+++ AccessModifiers/src/com/pack2/accessSpecifiers4.java
@@ -0,0 +1,11 @@
+package com.pack2;
+import com.pack1.*;
+public class accessSpecifiers4 {
+public static void main(String[] args) {
+		
+		pubaccessspecifiers obj = new pubaccessspecifiers(); 
+        obj.display();  
+		
+	}
+
+}
